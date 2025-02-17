6f699462789fcd8cae79214cbb12ff313e28ec28
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2025-02-17 17:39:13 +0600

    Update analytics.tsx

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -4,21 +4,29 @@ import { useEffect } from "react"
 import { usePathname, useSearchParams } from "next/navigation"
 import Script from "next/script"
 
+// Declare the gtag function as a property of the window object
+declare global {
+  interface Window {
+    gtag: (command: string, gtagId: string, config?: object) => void
+  }
+}
+
+export const GA_TRACKING_ID = "G-XXXXXXXXXX" // Replace with your actual Google Analytics 4 measurement ID
+
 export function Analytics() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
   useEffect(() => {
     const url = pathname + searchParams.toString()
-    // Push to dataLayer or call `gtag`
-    window.gtag("config", "G-XXXXXXXXXX", {
+    window.gtag("config", GA_TRACKING_ID, {
       page_path: url,
     })
   }, [pathname, searchParams])
 
   return (
     <>
-      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`} />
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
       <Script
         id="google-analytics"
         strategy="afterInteractive"
@@ -27,7 +35,7 @@ export function Analytics() {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-XXXXXXXXXX', {
+            gtag('config', '${GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
           `,
