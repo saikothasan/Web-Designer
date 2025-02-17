af0936faa038885c37622e81dea0ac3f5e7e01bb
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2025-02-17 17:33:54 +0600

    Update analytics.tsx

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -1,6 +1,39 @@
 "use client"
 
+import { useEffect } from "react"
+import { usePathname, useSearchParams } from "next/navigation"
+import Script from "next/script"
+
 export function Analytics() {
-  return null // Add your analytics code here
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    const url = pathname + searchParams.toString()
+    // Push to dataLayer or call `gtag`
+    window.gtag("config", "G-XXXXXXXXXX", {
+      page_path: url,
+    })
+  }, [pathname, searchParams])
+
+  return (
+    <>
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`} />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XXXXXXXXXX', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  )
 }
 
